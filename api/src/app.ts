24c2741c64e4672749650b37b9c65dd022444b24
glemoss <glemoss.dev@gmail.com>
2024-08-11 23:21:42 -0300

    feat: cors config

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,9 +1,13 @@
 import fastify from 'fastify'
 import { appRoutes } from './http/routes'
 import { ZodError } from 'zod'
-
+import fastifyCors from '@fastify/cors'
 export const app = fastify()
 
+app.register(fastifyCors, {
+  origin: 'http://localhost:4200',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+})
 app.register(appRoutes)
 
 app.setErrorHandler((error, _, reply) => {
